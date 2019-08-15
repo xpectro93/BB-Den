169355502c54025f22534ddc84de6bcca5b19a2c
xpectro93 <jonathanandrade@pursuit.org>
2019-08-15 17:57:23 -0400

    Utils updated to be able to be imported to other components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import NavBar from './NavBar.js'
 import { Switch, Route } from 'react-router-dom'
 import './CSS/App.css';
 import * as Util from './util/util'
-
+import Auth from './util/Auth'
 
 //Components
 import SignUp from './components/users/SignUp'
@@ -13,48 +13,40 @@ import Hooks from './Hooks.js'
 
 // axios.get(`https://www.googleapis.com/youtube/v3/search?key=${apikey}&part=snippet&q=${query}`)
 
-class App extends Component{
-  state = {
-  isLoggedIn:false,
-  userId:null
-  }
-  handleChange = e => {
-    this.setState({
-      [e.target.id] : e.target.value
-    })
+const App = props =>{
 
-  }
-  componentDidMount(){
-    this.isLoggedIn()
-  }
-  isLoggedIn = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userId, setUserId] = useState(null);
+
+  useEffect(()=> {
+    fetchIsLoggedIn()
+  },[])
+
+  const fetchIsLoggedIn = () => {
     // axios.get('/api/users/isLoggedIn')
     Util.isLoggedIn()
       .then(resp => {
-        console.log(resp)
+        setUserId(resp.data.id)
       })
   }
-  login = () => {
-  Util.login()
-  }
 
 
-  render(){
+
 
     return (
       <div className="App">
 
       <NavBar/>
           <Switch>
-        <Route path='/signup' render={(props) => <SignUp {...props} {...this.state}/> } />
+        <Route path='/signup' render={(props) => <SignUp {...props} isLoggedIn={isLoggedIn}/> } />
         <Route path='/login'  component={Login} />
         <Route path='/hooks' component={Hooks} />
           </Switch>
       </div>
     )
 
-  }
+
 
 }
 
-export default App;
+export default React.memo(App);
--- frontend/src/util/util.js
+++ frontend/src/util/util.js
@@ -1,7 +1,29 @@
 import axios from 'axios'
+import Auth from './Auth'
 
 //USER
 export const isLoggedIn = () => axios.get('/api/users/isLoggedIn')
 export const login = (userInfo) => axios.post('/api/users/login',userInfo)
 export const logout = () => axios.post('api/users/logout')
 export const newUser = (userInfo) => axios.post('api/users/new',userInfo)
+
+export const logoutUser = () => {
+    logout().then(() => {
+      Auth.deauthenticateUser();
+    })
+    .then(() => checkAuthenticateStatus());
+};
+export const checkAuthenticateStatus = (setIsLoggedIn,setUserId) => {
+  isLoggedIn().then(user => {
+    if (user.data.id === Auth.getToken()) {
+        setIsLoggedIn(Auth.isUserAuthenticated())
+        setUserId(Auth.getToken())
+    } else {
+      if (user.data.id) {
+        logoutUser();
+      } else {
+        Auth.deauthenticateUser();
+      }
+    }
+  });
+};
