ff5c4aa814c9e898330adece2ebb88b3e48aa211
xpectro93 <jonathanandrade@pursuit.org>
2019-09-27 02:51:34 -0400

    Text Express 3

--- backend/db/queries/index.js
+++
@@ -1,6 +0,0 @@
-const pgp = require('pg-promise')({});
-
-const db = pgp(process.env.DATABASE_URL || 'postgres://localhost/burrow');
-// const db = pgp('postgres://localhost:5432/burrow');
-
-module.exports = { db };
--- backendx/routes/users.js
+++ backendx/routes/users.js
@@ -1,9 +1,42 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const {
+        getAllUsers,
+        getAUser,
+        getAUserByusername,
+        createUser,
+        loginUser,
+        isLoggedIn,
+        logoutUser,
+        updateUser
+      } = require('../db/queries/usersQueries.js');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const passport = require("../auth/local");
+const { loginRequired } = require("../auth/helpers");
+
+
+
+router.get("/isLoggedIn", isLoggedIn);
+
+router.get("/username/:id",getAUserByusername);
+// * `GET /api/users/:id`
+//   * Get user based on id
+router.get('/:id',getAUser);
+
+// * `GET /api/users`
+//   * Get all users
+router.get('/', getAllUsers);
+
+// // * `POST /api/users`
+// //   * Create a new user
+router.post('/new', createUser);
+
+router.post("/logout", loginRequired, logoutUser);
+
+router.post("/login", passport.authenticate("local", {}), loginUser);
+
+// // * `PATCH /api/users/:user_id`
+// //   * Update a specific user
+router.patch('/:id', updateUser);
 
 module.exports = router;
